1eb24a657022d1b2d119c7e6673a9b497a0636c9
Parker Lauders <parkerlauders04@gmail.com>
2023-12-13 23:48:57 -0500

    Added list of options, with input/output of grade weight

--- CGPA_Calculator/CGPA_Calculator.cpp
+++ CGPA_Calculator/CGPA_Calculator.cpp
@@ -1,20 +1,140 @@
-// CGPA_Calculator.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // --- Grade Weight Variables ---
+    // Variables to hold the grade weight
+    float apValue, aVal, amVal, bpVal, bVal, bmVal, cpVal, cVal, cmVal, dpVal, dVal, dmVal, fVal = 0.0;
+
+    // Array to hold pointers to the 
+    float* gradeWeight[13] = { &apValue, &aVal, &amVal, &bpVal, &bVal, &bmVal, &cpVal, &cVal, &cmVal, &dpVal, &dVal, &dmVal, &fVal };
+    bool filled = false;
+
+    // --- Global Drive Variables ---
+    bool done = false;
+    int userInput = 0;
+    int globalIndex = 0;
+
+    // Main loop
+    while (!done) {
+        // Get user input for list options
+        // ... Loop until valid
+        do {
+            // Print options
+            std::cout << "\nList\n------------------------\n1. Grade Weight\n2. Current Grade Scale\n6. Exit\n------------------------";
+            std::cout << "\n>>";
+
+            // Get user input
+            std::cin >> userInput;
+        } while (userInput != 1 && userInput != 2 && userInput != 6);
+
+        // Act on user input
+        switch (userInput) {
+        case 1:
+            // Print selection title
+            std::cout << "\nGrade Weight\n";
+            std::cout << "------------------------\n";
+
+            // Loop through gathering
+            for (int i = 0; i <= 5; i++) {
+                // Skip E as it is not used as a letter grade
+                if (i == 4) {
+                    continue;
+                }
+
+                // Print plus, normal, and minus symbols
+                for (int j = 1; j <= 3; j++) {
+                    // Print the correct F
+                    if (i == 5 && (j == 1 || j == 3)) {
+                        continue;
+                    }
+
+                    // Print appropriate sign
+                    switch (j) {
+                    case 1:
+                        // Print
+                        std::cout << static_cast<char>(65 + i) << "+: ";
+
+                        // Get user input
+                        std::cin >> *gradeWeight[globalIndex];
+                        globalIndex++;
+                        break;
+                    case 2:
+                        // Print
+                        std::cout << " " << static_cast<char>(65 + i) << ": ";
+
+                        // Get user input
+                        std::cin >> *gradeWeight[globalIndex];
+                        globalIndex++;
+                        break;
+                    case 3:
+                        // Print
+                        std::cout << static_cast<char>(65 + i) << "-: ";
+
+                        // Get user input
+                        std::cin >> *gradeWeight[globalIndex];
+                        globalIndex++;
+                        break;
+                    } 
+                }
+            }
+            filled = true;
+            std::cout << "------------------------\n";
+
+            break;
+
+        case 2:
+            // Print selection title
+            std::cout << "\nCurrent Grade Weight\n";
+            std::cout << "------------------------\n";
+
+            // Check if filled
+            if (filled) {
+                // Reset globalIndex
+                globalIndex = 0;
+
+                // Loop through letter grades
+                for (int i = 0; i <= 5; i++) {
+                    // Skip E as it is not used as a letter grade
+                    if (i == 4) {
+                        continue;
+                    }
+
+                    // Print plus, normal, and minus symbols
+                    for (int j = 1; j <= 3; j++) {
+                        // Print the correct F
+                        if (i == 5 && (j == 1 || j == 3)) {
+                            continue;
+                        }
+
+                        // Print appropriate sign
+                        switch (j) {
+                        case 1:
+                            std::cout << static_cast<char>(65 + i) << "+: " << *gradeWeight[globalIndex] << "\n";
+                            globalIndex++;
+                            break;
+                        case 2:
+                            std::cout << " " << static_cast<char>(65 + i) << ": " << *gradeWeight[globalIndex] << "\n";
+                            globalIndex++;
+                            break;
+                        case 3:
+                            std::cout << static_cast<char>(65 + i) << "-: " << *gradeWeight[globalIndex] << "\n";
+                            globalIndex++;
+                            break;
+                        }
+                    }
+                }
+            }
+            else {
+                std::cout << "ERROR) List is empty, please add grade weight!\n";
+            }
+            
+            std::cout << "------------------------\n";
+
+            break;
+        case 6:
+            done = true;
+            break;
+        }
+    } 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
