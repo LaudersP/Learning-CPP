dd8c0e63eee92629b9a71da598feabf3e64feb8f
Parker Lauders <parkerlauders04@gmail.com>
2023-12-28 00:17:15 -0500

    Added needed functions for simple login process

--- Login_and_Registration_System/include/LoginSystem.hpp
+++ Login_and_Registration_System/include/LoginSystem.hpp
@@ -3,14 +3,33 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
+
+// --- OS decleration for terminal clearing ---
+#define WINDOWS
+#define LINUX
+#define MACOS
+
+#if defined(WINDOWS)
+#define CLEAR system("cls")
+#elif defined(LINUX) || defined(MACOS)
+#define CLEAR system('clear')
+#else
+#error "Unsupported OS"
+#endif
 
 class LoginSystem {
 private:
-	std::string* _filenamePtr;
+	std::string _filename;
+	std::string _username;
+	std::string _password;
+	bool CheckLogginInfo();
+	bool _logginFound;
 	
 public:
-	LoginSystem(std::string* filename);
+	LoginSystem(std::string filename);
 	void PrintDatabasePath();
+	void RequestLoggin();
 };
 
 #endif
