5d22fab813c421afeda5f1d4ffc28279611178ae
Parker Lauders <parkerlauders04@gmail.com>
2023-12-30 02:22:34 -0500

    Added functionality and login checking

--- Login_and_Registration_System/include/RegisterSystem.hpp
+++ Login_and_Registration_System/include/RegisterSystem.hpp
@@ -3,14 +3,39 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <cctype>
+
+// --- OS decleration for terminal clearing ---
+#define WINDOWS
+#define LINUX
+#define MACOS
+
+#if defined(WINDOWS)
+#define CLEAR system("cls")
+#elif defined(LINUX) || defined(MACOS)
+#define CLEAR system('clear')
+#else
+#error "Unsupported OS"
+#endif
 
 class RegisterSystem {
 private:
-	std::string* _filenamePtr;
+	std::string _filename;
+	std::string _username;
+	std::string _password;
+	bool CheckRegistrationInfo();
+	bool _length;
+	bool _hasLower;
+	bool _hasUpper;
+	bool _hasNum;
+	bool _hasSpecial;
+	void SaveRegistratedInfo();
 	
 public:
-	RegisterSystem(std::string* filename);
+	RegisterSystem(std::string filename);
 	void PrintDatabasePath();
+	void RequestRegistration();
 };
 
 #endif
